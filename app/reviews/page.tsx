13e12dc965201948691f49e988d0f340b9480788
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-21 15:07:49 -0400

    Add previews to reviews page

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,7 +1,13 @@
+import ReviewPreview from "@/components/reviews/ReviewPreview";
+
 export default function Reviews() {
     return (
         <main>
             <h1>Reviews</h1>
+            <ReviewPreview />
+            <ReviewPreview />
+            <ReviewPreview />
+            <ReviewPreview />
         </main>
     );
 }
