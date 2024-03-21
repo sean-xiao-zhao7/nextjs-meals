440d3e227a07ca357ac63c15880a62b1bdb2c115
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-21 15:12:20 -0400

    Add props to review preview

--- components/reviews/ReviewPreview.tsx
+++ components/reviews/ReviewPreview.tsx
@@ -1,12 +1,14 @@
-const ReviewPreview = () => {
+const ReviewPreview = ({ title, intro }: { title: string; intro: string }) => {
     return (
         <div>
-            <h1>Review Title</h1>
+            <h1>{title ? title : "Review Title"}</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
+                {intro
+                    ? intro
+                    : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut \
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris \
+                nisi ut aliquip ex ea commodo consequat."}
             </p>
         </div>
     );
