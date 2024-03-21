6c5f4354e2d0fe889dd8fff1e2d4a0e4e8986b4a
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-21 15:21:02 -0400

    Add sqlite

--- components/reviews/ReviewPreview.tsx
+++ components/reviews/ReviewPreview.tsx
@@ -1,4 +1,14 @@
-const ReviewPreview = ({ title, intro }: { title: string; intro: string }) => {
+import Link from "next/link";
+
+const ReviewPreview = ({
+    title,
+    intro,
+    slug,
+}: {
+    title: string;
+    intro: string;
+    slug: string;
+}) => {
     return (
         <div>
             <h1>{title ? title : "Review Title"}</h1>
@@ -10,6 +20,7 @@ const ReviewPreview = ({ title, intro }: { title: string; intro: string }) => {
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris \
                 nisi ut aliquip ex ea commodo consequat."}
             </p>
+            <Link href={`/reviews/${slug}`}>Check it out</Link>
         </div>
     );
 };
