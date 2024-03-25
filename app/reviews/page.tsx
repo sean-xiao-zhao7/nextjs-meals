a377762df2d015599ead89a00a06bcd78334107a
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-25 19:30:17 -0400

    Add fetchAllReviews

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,13 +1,24 @@
 import ReviewPreview from "@/components/reviews/ReviewPreview";
-
+import fetchAllReviews from "@/lib/fetch_reviews";
 export default function Reviews() {
+    const allReviews = fetchAllReviews();
     return (
         <main>
             <h1>Reviews</h1>
-            {/* <ReviewPreview />
-            <ReviewPreview />
-            <ReviewPreview />
-            <ReviewPreview /> */}
+            {allReviews.map(
+                (review: {
+                    title: string;
+                    description: string;
+                    joint_name: string;
+                    slug: string;
+                }) => (
+                    <ReviewPreview
+                        title={review.title}
+                        intro={review.description}
+                        slug={review.slug}
+                    />
+                )
+            )}
         </main>
     );
 }
