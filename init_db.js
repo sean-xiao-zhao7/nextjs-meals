c17a53edf006ce4dcce09adc723821c0829cc7f4
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-25 18:40:01 -0400

    Add dummy db data

--- init_db.js
+++ init_db.js
@@ -1,2 +1,40 @@
 const db = require("better-sqlite3")("seannodis_reviews.db");
 db.pragma("journal_mode = WAL");
+
+db.prepare(
+    `CREATE TABLE IF NOT EXISTS reviews (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    slug TEXT NOT NULL UNIQUE,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    joint_name TEXT NOT NULL
+)`
+).run();
+
+const dummy_reviews = [
+    {
+        title: "Pho",
+        description: "Pho",
+        joint_name: "Pho Ngoc Nen",
+    },
+    {
+        title: "Pho",
+        description: "Pho",
+        joint_name: "Pho Ngoc Nen",
+    },
+    {
+        title: "Pho",
+        description: "Pho",
+        joint_name: "Pho Ngoc Nen",
+    },
+    {
+        title: "Pho",
+        description: "Pho",
+        joint_name: "Pho Ngoc Nen",
+    },
+    {
+        title: "Pho",
+        description: "Pho",
+        joint_name: "Pho Ngoc Nen",
+    },
+];
