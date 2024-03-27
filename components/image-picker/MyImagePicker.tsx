ab7726e490178d0a4d2af6c2077c0c2c9c6ffa55
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-27 16:16:15 -0400

    Add picker logic

--- components/image-picker/MyImagePicker.tsx
+++ components/image-picker/MyImagePicker.tsx
@@ -1,22 +1,52 @@
+"use client";
+
 import { colors } from "@/app/styles/colors/colors";
+import Image from "next/image";
+import { useState } from "react";
 
 const MyImagePicker = () => {
+    const [imagePath, setImagePath] = useState<string>("");
+
     return (
-        <div
-            style={{
-                borderColor: "black",
-                borderRadius: 5,
-                backgroundColor: colors.mainColor,
-                padding: "1rem",
-            }}
-        >
-            <input
-                type="file"
-                id="image-path"
-                name="image-path"
-                accept="image/png, image/jpeg"
-            />
-        </div>
+        <>
+            <div
+                style={{
+                    borderColor: "black",
+                    borderRadius: 5,
+                    backgroundColor: colors.mainColor,
+                    padding: "1rem",
+                }}
+            >
+                <input
+                    type="file"
+                    id="image-path"
+                    name="image-path"
+                    accept="image/png, image/jpeg"
+                    onChange={(e: any) => {
+                        const file = e.target.files[0];
+                        if (file) {
+                            const fr = new FileReader();
+                            fr.onload = () => {
+                                setImagePath(fr.result as string);
+                            };
+                            fr.readAsDataURL(file);
+                        }
+                    }}
+                />
+            </div>
+            {imagePath ? (
+                <div
+                    style={{
+                        width: "300px",
+                        height: "300px",
+                        position: "relative",
+                        objectFit: "cover",
+                    }}
+                >
+                    <Image src={imagePath} alt="User picked image." />
+                </div>
+            ) : null}
+        </>
     );
 };
 
