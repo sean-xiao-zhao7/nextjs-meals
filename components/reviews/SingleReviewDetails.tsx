437855fb793786e1fce360da5e40467ec4a3edef
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-27 15:17:29 -0400

    Add not found

--- components/reviews/SingleReviewDetails.tsx
+++ components/reviews/SingleReviewDetails.tsx
@@ -1,7 +1,10 @@
 import { fetchOneReviewBySlug } from "@/lib/fetch_reviews";
+import { notFound } from "next/navigation";
 
 const SingleReviewDetails = async ({ slug }: { slug: string }) => {
     const review = await fetchOneReviewBySlug(slug);
+    if (!review) notFound();
+
     return (
         <>
             <h1>{review.title}</h1>
