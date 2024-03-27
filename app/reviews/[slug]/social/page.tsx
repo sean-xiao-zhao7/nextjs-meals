d5be97206348c8f5e58bbfb21b3f87b3140ec687
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-03-27 15:34:03 -0400

    Show slug in review social page

--- app/reviews/[slug]/social/page.tsx
+++ app/reviews/[slug]/social/page.tsx
@@ -1,7 +1,14 @@
-export default function Social() {
+import MyImagePicker from "@/components/image-picker/MyImagePicker";
+
+export default function Social({ params }: { params: { slug: string } }) {
+    console.log(params);
     return (
         <main>
-            <h1>Here are all the user shares!</h1>
+            <h1>Here are all the user shares for {params.slug}!</h1>
+            <div>
+                <h2>Add your image to {params.slug}</h2>
+                <MyImagePicker />
+            </div>
         </main>
     );
 }
--- components/image-picker/MyImagePicker.tsx
+++ components/image-picker/MyImagePicker.tsx
@@ -1,3 +1,17 @@
-const MyImagePicker = () => {};
+const MyImagePicker = () => {
+    return (
+        <>
+            <label htmlFor="image-path">Upload</label>
+            <div>
+                <input
+                    type="file"
+                    id="image-path"
+                    name="image-path"
+                    accept="image/png, image/jpeg"
+                />
+            </div>
+        </>
+    );
+};
 
 export default MyImagePicker;
