ac20fe177edd92d9571efcda64c1d2c051611d87
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-02 13:46:40 -0400

    Add corrent join names

--- db/init_db.js
+++ db/init_db.js
@@ -11,7 +11,8 @@ console.log(
     slug TEXT NOT NULL UNIQUE,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    joint_name TEXT NOT NULL
+    joint_name TEXT NOT NULL,
+    image_path TEXT
 )`
         )
         .run()
@@ -31,14 +32,14 @@ const dummy_reviews = [
         title: "IHOP",
         description:
             "The usual breakfast experience similar to other breakfast joints in the GTA. The name doesn't raise the bar, though menu has a few unique quirks. You got pancake, you got omelette, you got burrito. You don't get the wrapping arms around you embracing love you get down south from Mountain Mama on Country Road. You visit for breakfast craving, you get satisf",
-        joint_name: "Pho Ngoc Nen",
+        joint_name: "Ihop",
         slug: "ihop-breakfast",
     },
     {
         title: "Tsui Xiang Tsuen",
         description:
             "You crave dat Hong Kong mushu meat, you get hot and fresh meat and veggies steaming out of the wazzoo. You spend less for more, when few words are exchanged and understanding is achieved. You eat to fill and leftovers for next day, like Seinfeld episode.",
-        joint_name: "Pho Ngoc Nen",
+        joint_name: "Tsui Xiang Tsuen",
         slug: "txt-chinese",
     },
     {
@@ -46,14 +47,14 @@ const dummy_reviews = [
         description:
             "Hidden gem burger joint, legit burger fix for your cravings. Bacon crispy on point, price competitive, puts Burger King and McDonald's to shame. Milkshake game proper. Small sitting area, best have it to go. \
         Daily burger dosage is doable, reminiscent of pre-inflation values, which are greatly appreciated.",
-        joint_name: "Pho Ngoc Nen",
+        joint_name: "Matt Burger",
         slug: "mattb-burger",
     },
     {
         title: "Harvey's",
         description:
             "Very cozy Harv's, properly grilled burger with your choice of toppings. Large dining area with plants for the ambiance. Feels like old school shack. Burger cooked well, slushies mixed ok. Good to eat.",
-        joint_name: "Pho Ngoc Nen",
+        joint_name: "Harvey's",
         slug: "harvey-burger",
     },
 ];
--- lib/add_review.ts
+++ lib/add_review.ts
@@ -4,8 +4,6 @@ import ReviewType from "@/types/reviewType";
 import { addReviewDB } from "@/db/reviews/add_review";
 
 export const submitNewReviewHandler = async (formData: any) => {
-    "use server";
-
     const newReview: ReviewType = {
         title: formData.get("title"),
         description: formData.get("description"),
