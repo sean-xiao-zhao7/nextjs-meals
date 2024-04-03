18a1f9350a884e5df2209e2635699de1ead7c4f1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-03 14:38:43 -0400

    Adding required to form add review

--- app/reviews/add/page.tsx
+++ app/reviews/add/page.tsx
@@ -1,11 +1,7 @@
 import MyImagePicker from "@/components/image-picker/MyImagePicker";
 import { submitNewReviewHandler } from "@/lib/add_review";
 
-export default async function AddReview({
-    params,
-}: {
-    params: { slug: string };
-}) {
+export default async function AddReview() {
     return (
         <main>
             <h1>Adding a new review.</h1>
@@ -13,19 +9,29 @@ export default async function AddReview({
                 <form action={submitNewReviewHandler}>
                     Name
                     <br />
-                    <input type="text" name="title" id="name" />
+                    <input type="text" name="title" id="name" required />
                     <br />
                     Description
                     <br />
-                    <input type="text" name="description" id="description" />
+                    <input
+                        type="text"
+                        name="description"
+                        id="description"
+                        required
+                    />
                     <br />
                     Joint Name
                     <br />
-                    <input type="text" name="joint_name" id="joint_name" />
+                    <input
+                        type="text"
+                        name="joint_name"
+                        id="joint_name"
+                        required
+                    />
                     <br />
                     Slug
                     <br />
-                    <input type="text" name="slug" id="slug" />
+                    <input type="text" name="slug" id="slug" required />
                     <MyImagePicker />
                     <button type="submit">Submit</button>
                 </form>
