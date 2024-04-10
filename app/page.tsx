ee746ee2ccd90b2d12678a0f7eaf478074587894
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-10 13:30:34 -0400

    Layout image slides to right

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,24 @@ import Reviews from "./reviews/page";
 export default function App() {
     return (
         <main>
-            <h1>Welcome Foodies!</h1>
-            <p>
-                This is Seannodis&apos; restaurants review site. You can find
-                GTA food locations honest reviews.
-            </p>
-            <p>
-                The latest reviews are below, check Reviews on top to see all.
-            </p>
-            <div style={{ display: "flex", justifyContent: "center" }}>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                }}
+            >
+                <div>
+                    <h1>Welcome Foodies!</h1>
+                    <p>
+                        This is Seannodis&apos; restaurants review site. You can
+                        find GTA food locations honest reviews.
+                    </p>
+                    <p>
+                        The latest reviews are below, check Reviews on top to
+                        see all.
+                    </p>
+                </div>
                 <ImageSlideShow />
             </div>
             <Reviews />
