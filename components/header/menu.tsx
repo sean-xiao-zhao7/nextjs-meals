cea2fd837a296454e2a76061e7ad3066fbb170db
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-23 15:25:50 -0400

    Animate fixed menu coming in

--- components/header/menu.tsx
+++ components/header/menu.tsx
@@ -11,41 +11,38 @@ import { useState } from "react";
 
 export default function Menu({ fixed }: { fixed?: boolean }) {
     const path = usePathname();
-    const [menuFixed, setMenuFixed] = useState("");
+    const [menuFixed, setMenuFixed] = useState(false);
 
     const showFixedMenu = () => {
-        setMenuFixed("fixed");
+        setMenuFixed(true);
     };
 
     const hideFixedMenu = () => {
-        setMenuFixed("static");
+        setMenuFixed(false);
     };
 
     return (
         <motion.div
             onViewportLeave={showFixedMenu}
             onViewportEnter={hideFixedMenu}
-            layout
-            layoutScroll
         >
-            <div id="menu-container" className={fixed ? menuFixed : ""}>
-                <div id="menu-title-container">
-                    <div id="main-logo-container">
-                        <Link href="/" id="main-logo-link-container">
-                            <Image
-                                src={mainLogo}
-                                alt="Main logo"
-                                id="menu-logo"
-                                fill={true}
-                                sizes="(max-width: 768px) 10vw, (max-width: 1200px) 10vw, 5vw"
-                                priority={true}
-                            />
-                        </Link>
-                    </div>
-                    <Link href="/">
-                        <h1>SeanNodis&apos; Food Joints</h1>
+            <div id="menu-title-container">
+                <div id="main-logo-container">
+                    <Link href="/" id="main-logo-link-container">
+                        <Image
+                            src={mainLogo}
+                            alt="Main logo"
+                            id="menu-logo"
+                            fill={true}
+                            sizes="(max-width: 768px) 10vw, (max-width: 1200px) 10vw, 5vw"
+                            priority={true}
+                        />
                     </Link>
                 </div>
+                <Link href="/">
+                    <h1>SeanNodis&apos; Food Joints</h1>
+                </Link>
+
                 <menu>
                     <li>
                         <Link
@@ -89,6 +86,83 @@ export default function Menu({ fixed }: { fixed?: boolean }) {
                     </li>
                 </menu>
             </div>
+            {fixed && menuFixed && (
+                <motion.div
+                    id="menu-container"
+                    className="fixed"
+                    initial={{ height: 0, opacity: 0 }}
+                    animate={{ height: "auto", opacity: 1 }}
+                >
+                    <div id="menu-title-container">
+                        <div id="main-logo-container">
+                            <Link href="/" id="main-logo-link-container">
+                                <Image
+                                    src={mainLogo}
+                                    alt="Main logo"
+                                    id="menu-logo"
+                                    fill={true}
+                                    sizes="(max-width: 768px) 10vw, (max-width: 1200px) 10vw, 5vw"
+                                    priority={true}
+                                />
+                            </Link>
+                        </div>
+                        <Link href="/">
+                            <h1>SeanNodis&apos; Food Joints</h1>
+                        </Link>
+
+                        <menu>
+                            <li>
+                                <Link
+                                    href={"/reviews"}
+                                    className={
+                                        path.startsWith("/reviews")
+                                            ? "active"
+                                            : ""
+                                    }
+                                >
+                                    Reviews
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href={"/locations"}
+                                    className={
+                                        path.startsWith("/locations")
+                                            ? "active"
+                                            : ""
+                                    }
+                                >
+                                    Locations
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href={"/social"}
+                                    className={
+                                        path.startsWith("/social")
+                                            ? "active"
+                                            : ""
+                                    }
+                                >
+                                    Social
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href={"/gallery"}
+                                    className={
+                                        path.startsWith("/gallery")
+                                            ? "active"
+                                            : ""
+                                    }
+                                >
+                                    Gallery
+                                </Link>
+                            </li>
+                        </menu>
+                    </div>
+                </motion.div>
+            )}
         </motion.div>
     );
 }
