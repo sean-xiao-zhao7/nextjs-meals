87ec0c00fc4a39e99130f6ee3df15c0835676353
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-23 13:53:01 -0400

    Add motion

--- components/header/menu.js
+++ components/header/menu.js
@@ -1,44 +1,49 @@
 "use client";
 import Link from "next/link";
+
 import { usePathname } from "next/navigation";
 
 export default function Menu() {
     const path = usePathname();
 
     return (
-        <menu>
-            <li>
-                <Link
-                    href={"/reviews"}
-                    className={path.startsWith("/reviews") ? "active" : ""}
-                >
-                    Reviews
-                </Link>
-            </li>
-            <li>
-                <Link
-                    href={"/locations"}
-                    className={path.startsWith("/locations") ? "active" : ""}
-                >
-                    Locations
-                </Link>
-            </li>
-            <li>
-                <Link
-                    href={"/social"}
-                    className={path.startsWith("/social") ? "active" : ""}
-                >
-                    Social
-                </Link>
-            </li>
-            <li>
-                <Link
-                    href={"/gallery"}
-                    className={path.startsWith("/gallery") ? "active" : ""}
-                >
-                    Gallery
-                </Link>
-            </li>
-        </menu>
+        <>
+            <menu>
+                <li>
+                    <Link
+                        href={"/reviews"}
+                        className={path.startsWith("/reviews") ? "active" : ""}
+                    >
+                        Reviews
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href={"/locations"}
+                        className={
+                            path.startsWith("/locations") ? "active" : ""
+                        }
+                    >
+                        Locations
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href={"/social"}
+                        className={path.startsWith("/social") ? "active" : ""}
+                    >
+                        Social
+                    </Link>
+                </li>
+                <li>
+                    <Link
+                        href={"/gallery"}
+                        className={path.startsWith("/gallery") ? "active" : ""}
+                    >
+                        Gallery
+                    </Link>
+                </li>
+            </menu>
+        </>
     );
 }
